a62b41652092a45009c5e25a06b1d4bd8564d24f
Pafias <29665404+Pafias@users.noreply.github.com>
2024-11-12 11:31:46 +0100

    Update module path to github.com/Pafias/systray

--- webview_example/main.go
+++ webview_example/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/getlantern/systray"
-	"github.com/getlantern/systray/example/icon"
+	"github.com/Pafias/systray"
+	"github.com/Pafias/systray/example/icon"
 )
 
 func main() {
